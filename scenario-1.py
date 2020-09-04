37f9d1b4f31795e07196fd2002363b08367020f9
kono <kono@kononoMacBook-Pro.local>
2020-09-04 09:17:39 +0900

    implemented SequentialTaskSet for INSERT_DELETE operation

--- scenario-1.py
+++ scenario-1.py
@@ -1,17 +1,15 @@
-from locust import task, between
+from locust import task, between, SequentialTaskSet
 from locust.contrib.fasthttp import FastHttpUser
 import LocustUtil as Util
 
 device_instance = {}  # keep the status of device instance
 
+
 # The instance of HttpUser can represent one sensor/device.
 class Device1(FastHttpUser):
     wait_time = between(1, 2)
 
-    # instance of task can represent one type. (One device may send various data)
-    @task
-    def post_input(self):
-
+    def on_start(self):
         # retrieve the dev instance
         dev_id = Util.get_id(self, "dev")
         if dev_id in device_instance:
@@ -21,91 +19,57 @@ class Device1(FastHttpUser):
             dev.set_failure_rate(20, 10)
             device_instance[dev_id] = dev
 
-        # create json data
-        json_data = {
-            "id": dev_id,
-            "value": dev.next_int(),
-            "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
 
-        # send data to target
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input1",
-            data=Util.get_json_with_size(json_data, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name=Util.get_class_name(self)
-        )
-
-
-class Device2(FastHttpUser):
-    wait_time = between(1, 2)
-
-    # instance of task can represent one type. (One device may send various data)
     @task
-    def post_input(self):
+    class InsertDeleteSequence(SequentialTaskSet):
 
-        # retrieve the dev instance
-        dev_id = Util.get_id(self, "dev")
-        if dev_id in device_instance:
+        # instance of task can represent one type. (One device may send various data)
+        @task
+        def insert(self):
+
+            dev_id = Util.get_id(self.parent, "dev")
             dev = device_instance[dev_id]
-        else:
-            dev = Util.get_cyclic_random_sensor_data(init_elapsed_time=0, period=300, min=10, max=45, error_rate=0.1)
-            device_instance[dev_id] = dev
 
-        json_data = {
-            "id": Util.get_id(self, "dev"),
-            "value": dev.next_value(),
-            "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
+            # create json data
+            json_data = {
+                "id": dev_id,
+                "value": dev.next_int(),
+                "seq": dev.get_message_seq(),
+                "op" : "INSERT",
+                "Time": Util.get_current_time()
+            }
 
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input1",
-            data=Util.get_json_with_size(json_data, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name=Util.get_class_name(self)
-        )
+            # send data to target
+            response = self.client.post(
+                path="/api/v1/resources/topics//locust/input1",
+                data=Util.get_json_with_size(json_data, 1000),
+                auth=None,
+                headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
+                         "Content-Type": "application/json"},
+                name=Util.get_class_name(self)
+            )
 
+        @task
+        def delete(self):
 
-class Device3(FastHttpUser):
-    wait_time = between(1, 2)
-
-    # instance of task can represent one type. (One device may send various data)
-    @task
-    def post_input(self):
-
-        # retrieve the dev instance
-        dev_id = Util.get_id(self, "dev")
-        if dev_id in device_instance:
+            dev_id = Util.get_id(self.parent, "dev")
             dev = device_instance[dev_id]
-        else:
-            dev = Util.get_diminishing_random_sensor_data(init_value=100, half_life_time=30, restart_time=120, error_rate=0.01)
-            device_instance[dev_id] = dev
 
-        # Send requests at Xth second of every minute.
-#        Util.wait_until_xth_second(10)
+            # create json data
+            json_data = {
+                "id": dev_id,
+                "value": dev.next_int(),
+                "seq": dev.get_message_seq(),
+                "op" : "DELETE",
+                "Time": Util.get_current_time()
+            }
 
-        message = {
-            "id": Util.get_id(self, "dev"),   # "dev-xxxxx-n"
-            "value": dev.next_value(),
-            "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
-        json = Util.get_json_with_size(message, 1000)
-
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input1",
-            data=Util.get_json_with_size(message, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name=Util.get_class_name(self)
-        )
-
-#        print(response.request.headers)
-#       print("{}".format(json_data))
+            # send data to target
+            response = self.client.post(
+                path="/api/v1/resources/topics//locust/input1",
+                data=Util.get_json_with_size(json_data, 1000),
+                auth=None,
+                headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
+                         "Content-Type": "application/json"},
+                name=Util.get_class_name(self)
+            )
--- scenario-2.py
+++ scenario-2.py
@@ -2,99 +2,8 @@ from locust import task, between
 from locust.contrib.fasthttp import FastHttpUser
 import LocustUtil as Util
 
-
-# The instance of HttpUser can represent one sensor/device.
-class Device1(FastHttpUser):
-    wait_time = between(1, 2)
-    random1 = Util.get_simple_random_sensor_data(init_value=200, min=100, max=300, increment=10, error_rate=0.1)
-    random1.set_failure_rate(20, 10)
-
-    # instance of task can represent one type. (One device may send various data)
-    @task
-    def post_input(self):
-
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(10)
-
-        json_data = {
-            "id": Util.get_id(self, "dev"),
-            "value": self.random1.next_int(),
-            "seq": self.random1.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
-
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input1",
-            data=Util.get_json_with_size(json_data, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name="/locust/input1"
-        )
-
-
-class Device2(FastHttpUser):
-    wait_time = between(1, 2)
-    random2 = Util.get_cyclic_random_sensor_data(init_elapsed_time=0, period=300, min=10, max=45, error_rate=0.1)
-
-    # instance of task can represent one type. (One device may send various data)
-    @task
-    def post_input(self):
-
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(20)
-
-        json_data = {
-            "id": Util.get_id(self, "dev"),
-            "value": self.random2.next_value(),
-            "seq": self.random2.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
-
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input2",
-            data=Util.get_json_with_size(json_data, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name="/locust/input2"
-        )
-
-
-class Device3(FastHttpUser):
-    wait_time = between(1, 2)
-    random3 = Util.get_diminishing_random_sensor_data(init_value=100, half_life_time=30, restart_time=120, error_rate=0.01)
-
-    # instance of task can represent one type. (One device may send various data)
-    @task
-    def post_input(self):
-
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(30)
-
-        message = {
-            "id": Util.get_id(self, "dev"),   # "dev-xxxxx-n"
-            "value": self.random3.next_value(),
-            "seq": self.random3.get_message_seq(),
-            "Time": Util.get_current_time()
-        }
-        json = Util.get_json_with_size(message, 1000)
-
-        response = self.client.post(
-            path="/api/v1/resources/topics//locust/input3",
-            data=Util.get_json_with_size(message, 1000),
-            auth=None,
-            headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
-                     "Content-Type": "application/json"},
-            name="/locust/input3"
-        )
-
-
-from locust import task, between
-from locust.contrib.fasthttp import FastHttpUser
-import LocustUtil as Util
-
 device_instance = {}  # keep the status of device instance
+
 
 # The instance of HttpUser can represent one sensor/device.
 class Device1(FastHttpUser):
@@ -110,21 +19,17 @@ class Device1(FastHttpUser):
             dev = device_instance[dev_id]
         else:
             dev = Util.get_simple_random_sensor_data(init_value=200, min=100, max=300, increment=10, error_rate=0.1)
-            dev.set_failure_rate(20, 10)
             device_instance[dev_id] = dev
 
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(10)
-
-        # create json data
         json_data = {
-            "id": dev_id,
+            "id": Util.get_id(self, "dev"),
             "value": dev.next_int(),
+            "op": "UPSERT",
             "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
+            "time": Util.get_current_time()
         }
 
-        # send data to target
+
         response = self.client.post(
             path="/api/v1/resources/topics//locust/input2",
             data=Util.get_json_with_size(json_data, 1000),
@@ -137,6 +42,7 @@ class Device1(FastHttpUser):
 
 class Device2(FastHttpUser):
     wait_time = between(1, 2)
+    random2 = Util.get_cyclic_random_sensor_data(init_elapsed_time=0, period=300, min=10, max=45, error_rate=0.1)
 
     # instance of task can represent one type. (One device may send various data)
     @task
@@ -150,14 +56,12 @@ class Device2(FastHttpUser):
             dev = Util.get_cyclic_random_sensor_data(init_elapsed_time=0, period=300, min=10, max=45, error_rate=0.1)
             device_instance[dev_id] = dev
 
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(20)
-
         json_data = {
             "id": Util.get_id(self, "dev"),
-            "value": dev.next_value(),
+            "value": dev.next_int(),
+            "op": "UPSERT",
             "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
+            "time": Util.get_current_time()
         }
 
         response = self.client.post(
@@ -185,25 +89,21 @@ class Device3(FastHttpUser):
             dev = Util.get_diminishing_random_sensor_data(init_value=100, half_life_time=30, restart_time=120, error_rate=0.01)
             device_instance[dev_id] = dev
 
-        # Send requests at Xth second of every minute.
-        Util.wait_until_xth_second(30)
-
-        message = {
-            "id": Util.get_id(self, "dev"),   # "dev-xxxxx-n"
-            "value": dev.next_value(),
+        json_data = {
+            "id": Util.get_id(self, "dev"),
+            "value": dev.next_int(),
+            "op": "UPSERT",
             "seq": dev.get_message_seq(),
-            "Time": Util.get_current_time()
+            "time": Util.get_current_time()
         }
-        json = Util.get_json_with_size(message, 1000)
 
         response = self.client.post(
             path="/api/v1/resources/topics//locust/input2",
-            data=Util.get_json_with_size(message, 1000),
+            data=Util.get_json_with_size(json_data, 1000),
             auth=None,
             headers={"Authorization": "Bearer {}".format(Util.get_access_token()),
                      "Content-Type": "application/json"},
             name=Util.get_class_name(self)
         )
 
-#        print(response.request.headers)
-#       print("{}".format(json_data))
+
